efa57f9063ceeaf26e0d8b5718415b7ca48876a6
Idris Haji <hajimon_53@hotmail.com>
2023-10-03 13:02:36 +0100

    I have now added a id component for each ticket

--- app/tickets/[id]/page.jsx
+++ app/tickets/[id]/page.jsx
@@ -1,5 +1,27 @@
-export default function TicketDetails({ params }) {
-  const id = params.id;
+async function getTicket(id) {
+  const res = await fetch("http://localhost:4000/tickets/" + id, {
+    //This next function will return the revalidated tickets from the server
+    next: {
+      revalidate: 60,
+    },
+  });
 
-  return <div>{id}</div>;
+  return res.json();
+}
+
+export default async function TicketDetails({ params }) {
+  const ticket = await getTicket(params.id);
+
+  return (
+    <main>
+      <nav>
+        <h2>Ticket Details</h2>
+      </nav>
+      <div className="card">
+        <h3>{ticket.title}</h3>
+        <small>Created by {ticket.user_email}</small>
+        <p className={`pill ${ticket.priority}`}>{ticket.priority} priority</p>
+      </div>
+    </main>
+  );
 }
