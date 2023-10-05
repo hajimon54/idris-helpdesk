7775fd32301080a387d1128a590dd3b82d6a3bed
Idris Haji <hajimon_53@hotmail.com>
2023-10-05 12:46:56 +0100

    added a generatestaticparams function & also a 404 error page for any tickets that were called that do not exist within the database

--- app/tickets/TicketList.jsx
+++ app/tickets/TicketList.jsx
@@ -1,10 +1,29 @@
-async function getTickets() {
-  const res = await fetch("http://localhost:4000/tickets", {
+//export const dynamicParams = false; //returns a 404 page which returns an id of something different compared to the ticket pages that have already been generated
+export const dynamicParams = true;
+export async function generateStaticParams() {
+  const res = await fetch("ttp://localhost:4000/tickets");
+
+  const tickets = await res.json();
+
+  return tickets.map((ticket) => ({
+    id: ticket.id,
+  }));
+}
+
+import Link from "next/link";
+import { notFound } from "next/navigation";
+
+async function getTickets(id) {
+  const res = await fetch("http://localhost:4000/tickets/" + id, {
     //This next function will return the revalidated tickets from the server
     next: {
-      revalidate: 0, // use 0 to opt out of using cache.
+      revalidate: 60, // use 0 to opt out of using cache.
     },
   });
+
+  if (!res.ok) {
+    notFound();
+  }
 
   return res.json();
 }
@@ -16,7 +35,9 @@ export default async function TicketList() {
     <>
       {tickets.map((ticket) => (
         <div key={ticket.id} className="card my-5">
-          <h3>{ticket.title}</h3>
+          <Link href={`/tickets/${ticket.id}`}>
+            <h3>{ticket.title}</h3>
+          </Link>
           <p>{ticket.body.slice(0, 200)}...</p>
           <div className={`pill ${ticket.priority}`}>
             {ticket.priority} priority
