e8c07fd1349bee678b075cba70d44525cde721f7
Idris Haji <hajimon_53@hotmail.com>
2023-10-08 13:41:05 +0100

    I have added a 404 error page for tickets that dont exist within the database

--- app/tickets/TicketList.jsx
+++ app/tickets/TicketList.jsx
@@ -1,22 +1,7 @@
-export const dynamicParams = true; //returns a 404 page which returns an id of something different compared to the ticket pages that have already been generated
-//export const dynamicParams = true;
-export async function generateStaticParams() {
-  const res = await fetch("http://localhost:4000/tickets");
-
-  const tickets = await res.json();
-
-  console.log(tickets);
-  // return tickets;
-  return tickets.map((ticket) => ({
-    id: ticket.id,
-  }));
-}
-
 import Link from "next/link";
-//import { notFound } from "next/navigation";
 
-async function getTicket(id) {
-  const res = await fetch("http://localhost:4000/tickets/" + id, {
+async function getTickets() {
+  const res = await fetch("http://localhost:4000/tickets", {
     //This next function will return the revalidated tickets from the server
     next: {
       revalidate: 60, // use 0 to opt out of using cache.
@@ -26,18 +11,9 @@ async function getTicket(id) {
   return res.json();
 }
 
-console.log("hi");
-
 export default async function TicketList() {
-  const tickets = await getTicket();
-  console.log(tickets);
-  return tickets;
+  const tickets = await getTickets();
 
-  // {
-  //   tickets.length === 0 && (
-  //     <p className="text-center"> There are no open tickets, yay!</p>
-  //   );
-  // }
   return (
     <>
       {tickets.map((ticket) => (
--- app/tickets/[id]/page.jsx
+++ app/tickets/[id]/page.jsx
@@ -1,17 +1,37 @@
+export const dynamicParams = true; //returns a 404 page which returns an id of something different compared to the ticket pages that have already been generated
+//export const dynamicParams = true;
+export async function generateStaticParams() {
+  const res = await fetch("http://localhost:4000/tickets");
+
+  const tickets = await res.json();
+
+  //return tickets;
+  return tickets.map((ticket) => ({
+    id: ticket.id,
+  }));
+}
+
+import Link from "next/link";
+import { notFound } from "next/navigation";
+
 async function getTicket(id) {
   const res = await fetch("http://localhost:4000/tickets/" + id, {
     //This next function will return the revalidated tickets from the server
     next: {
-      revalidate: 60,
+      revalidate: 60, // use 0 to opt out of using cache.
     },
   });
+
+  if (!res.ok) {
+    notFound();
+  }
 
   return res.json();
 }
 
 export default async function TicketDetails({ params }) {
   const ticket = await getTicket(params.id);
-  console.log(ticket);
+  //console.log(ticket);
 
   return (
     <main>
